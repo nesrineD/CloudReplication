6e12275c0b99a4eeaac18356da1560d8a6b42437
nesrineD <nesrine.doghri1@gmail.com>
2015-06-30 18:09:02 +0200

    port

--- src/de/tub/aec/server/HermesServer.java
+++ src/de/tub/aec/server/HermesServer.java
@@ -13,43 +13,41 @@ import edu.kit.aifb.dbe.hermes.RequestHandlerRegistry;
 import edu.kit.aifb.dbe.hermes.SimpleFileLogger;
 
 /**
- * The main Server Class
- * It registers the handlers and starts 
- * 
+ * The main Server Class It registers the handlers and starts
+ *
  */
 public class HermesServer {
+	
 	private static RequestHandlerRegistry req = null;
-
+	
 	static {
 		PropertyConfigurator.configure("resources/log4j.properties");
 		SimpleFileLogger.getInstance();
 		req = RequestHandlerRegistry.getInstance();
 	}
 	
-
 	public static RequestHandlerRegistry getRequestHandlerRegistry() {
-		if(req == null) {
+		if (req == null) {
 			req = RequestHandlerRegistry.getInstance();
 		}
 		return req;
 	}
-	
 	
 	public static void main(String[] args) {
 		CreateHandler ch = new CreateHandler();
 		UpdateHander uh = new UpdateHander();
 		ReadHandler rh = new ReadHandler();
 		DeleteHandler dh = new DeleteHandler();
-		
+
 		HermesServer.getRequestHandlerRegistry().registerHandler("create", ch);
 		HermesServer.getRequestHandlerRegistry().registerHandler("update", uh);
 		HermesServer.getRequestHandlerRegistry().registerHandler("read", rh);
 		HermesServer.getRequestHandlerRegistry().registerHandler("delete", dh);
-		
+
 		Receiver receiver = null;
-		int port = 8081;
+		int port = 5000;
 		try {
-			
+
 			receiver = new Receiver(port, 3, 3);
 			receiver.start();
 		} catch (IOException e) {
--- src/de/tub/aec/util/ConfigureHelper.java
+++ src/de/tub/aec/util/ConfigureHelper.java
@@ -7,35 +7,37 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-/** 
+/**
  * This class is used to store the informations of the property file into a map
  *
  */
 
 public class ConfigureHelper {
-	public static int PORT = 8081;					   
-	public static int TIME_OUT = 10000;			
+	
+	public static int TIME_OUT = 10000;
 	
 	public static String SYNC = "sync";
 	public static String ASYNC = "async";
-	
-	public static String SELF_NAME = "nodeA";			
+
+	public static String SELF_NAME = "nodeA";
 	
 	/**
-	 * Stores the ip addresses, nodeId mapping in a map 
+	 * Stores the ip addresses, nodeId mapping in a map
+	 * 
 	 * @return the server map: key= node ID, value=private IP address
 	 */
-	
-	public static Map<String, String> PropertyParser() throws IOException {
+
+	public static Map<String, String> PropertyParser()
+			throws IOException {
 		InputStream input = new FileInputStream("resources/ip_assignment.properties");
 		Properties property = new Properties();
-        property.load(input);
-        Map<String, String> servermap = new HashMap<String, String>();
-        servermap.put("nodeB", property.getProperty("nodeB"));
-        servermap.put("nodeC", property.getProperty("nodeC"));
-        servermap.put("nodeA", property.getProperty("nodeA"));
-        return servermap;
-        		
+		property.load(input);
+		Map<String, String> servermap = new HashMap<String, String>();
+		servermap.put("nodeB", property.getProperty("nodeB"));
+		servermap.put("nodeC", property.getProperty("nodeC"));
+		servermap.put("nodeA", property.getProperty("nodeA"));
+		return servermap;
+		
 	}
-
+	
 }
